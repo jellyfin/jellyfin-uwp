1ed3a1ba74999370d7fc3bb8c3bb48317ba2bd12
chaosinnovator <thomas.boland@outlook.com>
2019-12-08 16:07:32 -0800

    Added basic functionality

---
+++ jellyfin-uwp/SettingsStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jellyfin_uwp
+{
+    public class SettingsStore
+    {
+
+        public string AppURL = "";
+
+        public bool AppURLValid = false;
+        public bool AppURLIsJellyfin = false;
+
+    }
+}
--- jellyfin-uwp/WebView.xaml.cs
+++ jellyfin-uwp/WebView.xaml.cs
@@ -13,33 +13,20 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace jellyfin_uwp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class WebView : Page
     {
-        public static MainPage mainPageReference;
-        public static SettingsStore globalSettingsStore;
-
-        public MainPage()
+        public WebView()
         {
             this.InitializeComponent();
 
-            mainPageReference = this;
-
-            globalSettingsStore = new SettingsStore();
-
-            if (!globalSettingsStore.AppURLValid)
-            {
-                JellyfinFrame.Navigate(typeof(AppSettings));
-            }
-            else {
-                JellyfinFrame.Navigate(typeof(WebView));
-            }
+            JellyfinWebView.Navigate(new Uri(MainPage.globalSettingsStore.AppURL));
         }
     }
 }
